d724fb168e82c962363edc44d14dcb94ca494d03
Kelly <kellyhowes83@gmail.com>
2024-10-24 10:25:44 +0100

    Completed challenge

--- lib/postcode_checker.py
+++ lib/postcode_checker.py
@@ -4,7 +4,5 @@ class PostcodeChecker():
     def check(self, postcode):
         if postcode is None:
             return False
-        return re.match(
-            r"^[a-z]\d[a-z\d]?\s*\d[a-z]{2}$",
-            postcode,
-            re.IGNORECASE) is not None
+        return re.match(r"^([a-z]{2}\d[a-z]\s\d[a-z]{2})",postcode,re.IGNORECASE) is not None
+
